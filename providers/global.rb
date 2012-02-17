08f48bbc8767420d6569fc6c6667c84a551eac55
Nikita Simakov <everm1nd.mail@gmail.com>
2012-02-17 18:15:11 +0400

    rbenv_global provider and resource

---
+++ providers/global.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook Name:: rbenv
+# Provider:: ruby
+#
+# Author:: Jamie Winsor (<jwinsor@riotgames.com>)
+# Copyright 2011, Riot Games
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include Chef::Mixin::Rbenv
+
+action :activate do
+  execute "export ruby version in /opt/rbenv/version" do
+    command "echo '#{new_resource.name}' > /opt/rbenv/version"
+  end
+  # if !@ruby.force && ruby_version_installed?(@ruby.name)
+  #   Chef::Log.debug "rbenv_ruby[#{@ruby.name}] is already installed so skipping"
+  # else
+  #   Chef::Log.info "rbenv_ruby[#{@ruby.name}] is building, this may take a while..."
+
+  #   start_time = Time.now
+
+  #   out = rbenv_command("install #{@ruby.name}")
+
+  #   unless out.exitstatus == 0
+  #     raise Chef::Exceptions::ShellCommandFailed, "\n" + out.format_for_exception
+  #   end
+
+  #   Chef::Log.debug("rbenv_ruby[#{@ruby.name}] build time was #{(Time.now - start_time)/60.0} minutes.")
+  # end
+end
